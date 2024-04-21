215d3ebe88d264b67ee5af0ca4ccc3e0e1343817
Aaryan Rawat <66914091+AaryanRawat@users.noreply.github.com>
2024-04-21 00:30:37 -0400

    added basic communication with firebase

--- backend/api.ts
+++ backend/api.ts
@@ -1,8 +1,31 @@
 import { db } from './firebaseConfig'
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, getDocs } from 'firebase/firestore'
 
 interface Book {
     title: string;
     author: string;
     cover: string;
+}
+
+const fetchBooks = async (): Promise<Book[]> => {
+    try{
+        const booksCollection = collection(db, 'books');
+        const bookSnapshot = await getDocs(booksCollection);
+        const bookList = bookSnapshot.docs.map(doc => doc.data() as Book);
+
+        return bookList;
+    }
+    catch (e) {
+        throw(e);
+    }
+};
+
+const addBook = async (book: Book) => {
+    try{
+        const docRef = await addDoc(collection(db, 'books'), book);
+        console.log('Document written with ID: ', docRef.id);
+    }
+    catch (e) {
+        console.error('Error adding book: ', e);
+    }
 }
