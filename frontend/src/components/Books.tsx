61eb8a01c09a1d6339d9d6b1e1953660af7a0ee5
Aaryan Rawat <66914091+AaryanRawat@users.noreply.github.com>
2024-04-20 23:47:30 -0400

    created Books component

---
+++ frontend/src/components/Books.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, CardContent, CardMedia, Typography, Button } from '@mui/material';
+
+const Books = () => {
+  return (
+    <div className='booksContainer'>
+        
+    </div>
+  )
+}
+
+export default Books
