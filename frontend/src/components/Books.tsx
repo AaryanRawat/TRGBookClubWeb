4537000fae0ff9421c4c993084fe59d76bc3fe5d
Aaryan Rawat <66914091+AaryanRawat@users.noreply.github.com>
2024-05-12 11:58:42 -0400

    refactor

--- frontend/src/components/Books.tsx
+++ frontend/src/components/Books.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from 'react';
 import { Card, CardContent, CardMedia, Typography, Button } from '@mui/material';
+
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../api/firebaseConfig'
 
 const Books = () => {
   return (
